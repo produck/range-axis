7abb989a8eddf917c47d0f306891f3c43634fdc9
chaoslee <chaos2lee@qq.com>
2023-09-06 16:56:46 +0800

    workspace: [ADDED] ::Union()

--- src/Union.mjs
+++ src/Union.mjs
@@ -1,9 +1,25 @@
-import * as Structure from './Structure/index.mjs';
+import { Axis } from './Structure/index.mjs';
+
+const ASC = (a, b) => a[0] - b[0] || a[1] - b[1];
+const HEAD = [-Infinity, -Infinity];
+const TAIL = [Infinity, Infinity];
 
 export const operator = (_a, _b) => {
-	const a = Structure.Axis.normalize(_a);
-	const b = Structure.Axis.normalize(_b);
-	const result = [];
+	const a = Axis.normalize(_a), b = Axis.normalize(_b);
+	const target = [...HEAD], result = [];
+
+	for (const range of [...a, ...b, TAIL].sort(ASC)) {
+		if (range[0] > target[1]) {
+			result.push([...target]);
+			target[0] = range[0];
+		}
+
+		if (range[1] > target[1]) {
+			target[1] = range[1];
+		}
+	}
+
+	result.shift();
 
 	return result;
 };
---
+++ src/index.mjs
@@ -0,0 +1,2 @@
+export * from './Structure/index.mjs';
+export { operator as Union } from './Union.mjs';
