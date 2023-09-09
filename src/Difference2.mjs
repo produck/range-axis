75e951b4108421e0bd9e8098677672c3783002bf
chaoslee <chaos2lee@qq.com>
2023-09-09 14:55:57 +0800

    workspace: [FIXED] difference2

--- src/Difference2.mjs
+++ src/Difference2.mjs
@@ -4,41 +4,29 @@ const TOP = 0, FROM = 0, TO = 1;
 
 export const operator = Utils.defineOperator((A, B, Result) => {
 	while (A.length > 0) {
-		if (B.length === 0) { // 无可减
-			return Result.push(...A.splice(0));
+		if (B.length === 0) {
+			return Result.push(...A);
 		}
 
-		let [from, to] = A[TOP];
+		if (B[TOP][FROM] > A[TOP][TO]) {
+			Result.push(A.shift());
+		} else while (B.length > 0 && A.length > 0 && B[TOP][FROM] < A[TOP][TO]) {
+			const [from, to] = B[TOP];
 
-		if (B[TOP][FROM] > to) { // B完全领先
-			Result.push(A[TOP]);
-			A.shift();
-			continue;
-		}
+			if (to < A[TOP][TO]) {
+				B.shift();
+			}
 
-		while (B.length > 0 && B[TOP][FROM] < to) { // B尚未领先
-			if (B[TOP][FROM] < from) {
-				if (B[TOP][TO] < from) {
-					B.shift();
-				} else if (B[TOP][TO] > from && B[TOP][TO] < to) {
-					from = B[TOP][TO];
-					A[TOP][FROM] = from, A[TOP][TO] = to;
-					B.shift();
-				} else if (B[TOP][TO] > to) {
-					A.shift();
-					break;
-				}
-			} else if (B[TOP][FROM] > from) {
-				Result.push([from, B[TOP][FROM]]);
+			if (from > A[TOP][FROM]) {
+				Result.push([A[TOP][FROM], from]);
+			}
 
-				if (B[TOP][TO] < to) {
-					from = B[TOP][TO];
-					A[TOP][FROM] = from, A[TOP][TO] = to;
-					B.shift();
-				} else if (B[TOP][TO] > to) {
-					A.shift();
-					break;
-				}
+			if (to > A[TOP][FROM] && to < A[TOP][TO]) {
+				A[TOP][FROM] = to;
+			}
+
+			if (to > A[TOP][TO]) {
+				A.shift();
 			}
 		}
 	}
