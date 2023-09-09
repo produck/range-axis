1c1a47c1d318f4f2b4dac682f29fdfd678f2af14
chaoslee <chaos2lee@qq.com>
2023-09-09 17:02:27 +0800

    workspace: [FIXED] difference2: optimization

--- src/Difference2.mjs
+++ src/Difference2.mjs
@@ -3,33 +3,25 @@ import * as Utils from './Utils.mjs';
 const TOP = 0, FROM = 0, TO = 1;
 
 export const operator = Utils.defineOperator((A, B, Result) => {
-	let a = A[TOP], b = B[TOP];
-
 	while (A.length > 0) {
 		if (B.length === 0) {
 			return Result.push(...A);
 		}
 
-		if (b[FROM] > a[TO]) {
+		if (B[TOP][FROM] >= A[TOP][TO]) {
 			Result.push(A.shift());
-		} else while (B.length > 0 && A.length > 0 && b[FROM] < a[TO]) {
-			if (b[FROM] > a[FROM]) {
-				Result.push([a[FROM], b[FROM]]);
+		}
+
+		while (B.length > 0 && A.length > 0 && B[TOP][FROM] < A[TOP][TO]) {
+			if (B[TOP][FROM] > A[TOP][FROM]) {
+				Result.push([A[TOP][FROM], B[TOP][FROM]]);
 			}
 
-			if (b[TO] > a[FROM] && b[TO] < a[TO]) {
-				a[FROM] = b[TO];
+			if (B[TOP][TO] > A[TOP][FROM] && B[TOP][TO] < A[TOP][TO]) {
+				A[TOP][FROM] = B[TOP][TO];
 			}
 
-			if (b[TO] < a[TO]) {
-				B.shift();
-			}
-
-			if (b[TO] > a[TO]) {
-				A.shift();
-			}
-
-			a = A[TOP], b = B[TOP];
+			(B[TOP][TO] <= A[TOP][TO] ? B : A).shift();
 		}
 	}
 });
